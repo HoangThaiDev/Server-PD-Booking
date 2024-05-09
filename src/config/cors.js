086322ab57d871d1c0bd7c9218c673f5a1e66ee2
HoangThaiDev <thaindev2000@gmail.com>
2024-05-09 21:33:03 +0700

    fix cors.js

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,12 +1,18 @@
 // Import Modules
 const { WHITELIST_DOMAINS } = require("../utils/constants");
+const env = require("../config/enviroment");
 
 exports.corsOptions = {
   origin: function (origin, callback) {
+    if (env.BUILD_MODE === "dev") {
+      return callback(null, true);
+    }
+
     if (WHITELIST_DOMAINS.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
+      return callback(null, true);
     }
   },
+
+  optionsSuccessStatus: 200,
+  credentials: true,
 };
