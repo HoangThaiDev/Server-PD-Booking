d0b5f7d86d93ad04770f1d4707593b100963f6de
HoangThaiDev <thaindev2000@gmail.com>
2024-05-09 22:10:24 +0700

    Fix server lan 4

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,2 @@
 /** Những domain được phép truy cập tới tài nguyên của server */
-exports.WHITELIST_DOMAINS = [
-  "http://localhost:3000",
-  "https://paradise-booking.vercel.app/",
-];
+exports.WHITELIST_DOMAINS = ["https://paradise-booking.vercel.app/"];
