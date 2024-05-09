0576eb07e51ed11a8a5e9c5aa47d764c5a6c3928
HoangThaiDev <thaindev2000@gmail.com>
2024-05-09 21:02:44 +0700

    fix domain

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,12 +1,8 @@
 // Import Modules
 const { WHITELIST_DOMAINS } = require("../utils/constants");
-const env = require("../config/enviroment");
 
 exports.corsOptions = {
   origin: function (origin, callback) {
-    if (env.BUILD_MODE === "dev") {
-      return callback(null, true);
-    }
     if (WHITELIST_DOMAINS.includes(origin)) {
       callback(null, true);
     } else {
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,5 @@
 /** Những domain được phép truy cập tới tài nguyên của server */
-exports.WHITELIST_DOMAINS = ["https://paradise-booking.vercel.app/"];
+exports.WHITELIST_DOMAINS = [
+  "http://localhost:3000",
+  "https://paradise-booking.vercel.app/",
+];
