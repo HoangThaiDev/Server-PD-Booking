405d294073550360271f4310db02e2006d56ba85
HoangThaiDev <thaindev2000@gmail.com>
2024-05-09 20:16:02 +0700

    Fix lan 2 Cors

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,2 @@
 /** Những domain được phép truy cập tới tài nguyên của server */
-exports.WHITELIST_DOMAINS = [
-  "http://localhost:3000",
-  "https://vercel-pd-booking.vercel.app",
-];
+exports.WHITELIST_DOMAINS = ["https://vercel-pd-booking.vercel.app"];
