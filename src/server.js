292434d8fe594e9479e7fe04ddb88e03931b55f9
HoangThaiDev <thaindev2000@gmail.com>
2024-05-09 18:29:00 +0700

    Upload Server.js

--- src/server.js
+++ src/server.js
@@ -17,23 +17,23 @@ app.use(express.json());
 app.use(cors());
 
 // Create Server DBS + Connect Server
-mongooseConnect(() => {
-  if (env.BUILD_MODE === "production") {
-    // Enviroment Production is suporting Render
+if (env.BUILD_MODE === "production") {
+  mongooseConnect(() => {
     app.listen(process.env.PORT, (err) => {
       console.log(
-        `Production: Hi ${env.AUTHOR}. Start server at port: ${rocess.env.PORT}`
+        `Production: Hi ${env.AUTHOR}. Start server at port: ${process.env.PORT}`
       );
     });
-  } else {
-    // Enviroment Local Dev
+  });
+} else {
+  mongooseConnect(() => {
     app.listen(env.LOCAL_APP_PORT, (err) => {
       console.log(
         `LocalDev: Hi ${env.AUTHOR}. Start server at host: ${env.LOCAL_APP_HOST} and port: ${env.LOCAL_APP_PORT}`
       );
     });
-  }
-});
+  });
+}
 
 /// Create Routes
 app.use("/users", userRouter);
