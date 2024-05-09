766a53ee004e351550d887a8548722e3bebcbb14
HoangThaiDev <thaindev2000@gmail.com>
2024-05-09 19:49:24 +0700

    Add Domain FE

--- src/config/cors.js
+++ src/config/cors.js
@@ -4,7 +4,6 @@ const { WHITELIST_DOMAINS } = require("../utils/constants");
 exports.corsOptions = {
   origin: function (origin, callback) {
     if (WHITELIST_DOMAINS.includes(origin)) {
-      console.log("sai localhost");
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,5 @@
 /** Những domain được phép truy cập tới tài nguyên của server */
-exports.WHITELIST_DOMAINS = ["http://localhost:3000"];
+exports.WHITELIST_DOMAINS = [
+  "http://localhost:3000",
+  "https://pd-booking-resort.vercel.app",
+];
