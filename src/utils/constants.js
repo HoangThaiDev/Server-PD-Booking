15631b7fe6d13c0f9d840c00607e275a8c6b87e0
HoangThaiDev <thaindev2000@gmail.com>
2024-05-09 21:17:45 +0700

    Fix server lan 2

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 /** Những domain được phép truy cập tới tài nguyên của server */
 exports.WHITELIST_DOMAINS = [
   "http://localhost:3000",
-  "https://paradise-booking.vercel.app/",
+  "https://paradise-booking.vercel.app",
 ];
